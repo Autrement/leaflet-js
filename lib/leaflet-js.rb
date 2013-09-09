6682dd3d218a307cb73c798419d4549b2db35fd3
Colin Darie <colindarie@gmail.com>
2013-09-09 14:26:48 +0200

    Update to leaflet 0.6.4 without submodules to prepare images paths fixes

--- lib/leaflet-js.rb
+++ lib/leaflet-js.rb
@@ -1,8 +1,4 @@
 module Leaflet
   class Engine < Rails::Engine
-    initializer :append_assets_path do |app|
-      # Javascripts
-      app.config.assets.paths << File.join(config.root, 'lib', 'leaflet', 'src')
-    end
   end
 end
